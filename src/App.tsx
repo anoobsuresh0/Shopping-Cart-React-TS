e086f9dc3c85a9f255d804f4d7f655b2830b3ce4
anoob-perleybrook <anoob.suresh@perleybrook.com>
2024-01-21 12:28:42 +0530

    Fea: NavBar and Shoppping cart icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+    <NavBar />
+    <Container className="mb-4" >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </>
+  );
 }
 export default App;
